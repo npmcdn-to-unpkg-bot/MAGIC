f7a63fd8aaf979b962be6d92cc01a33d5750435f
Alejandro Molina <Alejandro_molina_acosta@brown.edu>
2016-05-10 03:48:55 -0400

    update

--- app/models/profile.ts
+++ app/models/profile.ts
@@ -1,8 +1,3 @@
 export class Profile {
-  constructor(
-    public id:string,
-    public token:string,
-    public email:string,
-    public name:string,
-    public photo:string) { }
+  constructor() { }
 }
