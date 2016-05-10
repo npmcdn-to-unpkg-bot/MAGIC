743cc7430ae93080c4feb189598d3bba6a1ca8fd
Aravind Elangovan <filch20@gmail.com>
2016-05-10 02:49:13 -0400

    Changed db.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://eronning:password@ds033143.mlab.com:33143/database_awesome' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://eronning:password@ds051720.mlab.com:51720/database_awesome' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
